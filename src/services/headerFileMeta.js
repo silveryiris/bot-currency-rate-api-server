63a097632c50609c9a21cc1fab4a2d0ff2832e58
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-19 00:14:10 +0800

    Remove file length response

--- src/services/headerFileMeta.js
+++ src/services/headerFileMeta.js
@@ -1,15 +1,13 @@
 function getFileMetaFromHeaders(headers) {
   const [date] = headers["date"]
   const [fileType] = headers["content-type"]
-  const [fileLength] = headers["content-length"]
   const [fileNameRaw] = headers["content-disposition"]
   const fileName = fileNameRaw.match(/"(.*?)"/)[0].replace(/['|"]/g, "")
 
   return {
     date,
     fileName,
-    fileType,
-    fileLength
+    fileType
   }
 }
 

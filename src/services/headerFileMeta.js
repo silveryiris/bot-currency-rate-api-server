060d6f77a49be36c9bf2ba42d6c0b9b893e7787c
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-19 00:20:44 +0800

    Remove source file type on response

--- src/services/headerFileMeta.js
+++ src/services/headerFileMeta.js
@@ -1,13 +1,11 @@
 function getFileMetaFromHeaders(headers) {
   const [date] = headers["date"]
-  const [fileType] = headers["content-type"]
   const [fileNameRaw] = headers["content-disposition"]
   const fileName = fileNameRaw.match(/"(.*?)"/)[0].replace(/['|"]/g, "")
 
   return {
     date,
-    fileName,
-    fileType
+    fileName
   }
 }
 

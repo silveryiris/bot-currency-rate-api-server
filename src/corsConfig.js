079f1ae5426713697e71aeead70ff2cb20656d06
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-18 22:51:09 +0800

    Rewrite core config functions

--- src/corsConfig.js
+++ src/corsConfig.js
@@ -1,7 +1,22 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+const formatSourceString = str => {
+  return str.split(",").map(x => x.trim())
+}
+
+const whiteListSorce = process.env.CORS_WHITE_LIST || "default"
+const httpMethodSorce = process.env.CORS_AllOWED_METHODS || "default"
+const headerSource = process.env.CORS_AllOWED_HEADERS || "default"
+
+const whiteList = whiteListSorce === "default" ? true : formatSourceString(whiteListSorce)
+const allowedMethods = httpMethodSorce === "default" ? "GET" : formatSourceString(httpMethodSorce)
+const allowedHeaders = headerSource === "default" ? ["Content-Type", "Authorization"] : formatSourceString(headerSource)
+
 const options = {
-  origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
-  methods: "GET",
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: whiteList,
+  methods: allowedMethods,
+  allowedHeaders: allowedHeaders
 }
 
 export default options
